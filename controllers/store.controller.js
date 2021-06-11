9a84da56e7771a9b9c47703632b1e7662f0f4b03
ajoncode <ajoncode@gmail.com>
2021-06-11 13:55:21 +0530

    Saving Stores and using Mixins

--- controllers/store.controller.js
+++ controllers/store.controller.js
@@ -1,8 +1,18 @@
 exports.homePage = (req, res) => {
-    console.info('req.name: ', req.name);
-    res.render('hello', {
-        title: 'I Love Food! ',
-        name: 'AJ',
-        dog: req.query.dog || 'MAX',
-    });
+	console.info('req.name: ', req.name);
+	res.render('hello', {
+			title: 'I Love Food! ',
+			name: 'AJ',
+			dog: req.query.dog || 'MAX',
+	});
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', {
+		title: 'Add Store',
+	});
+};
+
+exports.createStore = (req, res) => {
+	res.json(req.body);
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/store.controller');
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
