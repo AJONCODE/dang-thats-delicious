9b441196b300961c8cd106221591fd694925ebf7
ajoncode <ajoncode@gmail.com>
2021-06-12 13:58:43 +0530

    Creating an Editing Flow for Stores

--- controllers/store.controller.js
+++ controllers/store.controller.js
@@ -39,3 +39,32 @@ exports.getStores = async (req, res) => {
 		stores,
 	})
 };
+
+exports.editStore = async (req, res) => {
+	const store = await Store.findOne({ _id: req.params.id });
+
+	// TODO: Confirm they are the owner of the store
+
+	res.render('editStore', {
+		title: `Edit ${store.name}`,
+		store,
+	});
+};
+
+exports.updateStore = async (req, res) => {
+	let store = await Store.findOneAndUpdate(
+		{ _id: req.params.id },
+		req.body,
+		{
+			new: true,
+			runValidators: true,
+		},
+	).exec();
+
+	req.flash(
+		'success',
+		`Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`
+	);
+
+	res.redirect(`/stores/${store._id}/edit`);
+};
