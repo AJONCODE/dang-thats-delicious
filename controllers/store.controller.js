f321bed8ebe79ab4f517a0e2e24c37dd6d0b38e1
ajoncode <ajoncode@gmail.com>
2021-06-13 02:05:14 +0530

    Uploading and Resizing Images with Middleware

--- controllers/store.controller.js
+++ controllers/store.controller.js
@@ -3,14 +3,43 @@ const mongoose = require('mongoose');
 // reference it of the mongoose. Because mongoose use the concept of the Singleton, which
 // allows us to only import our models once and reference it anywhere in our application.
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
 
-// exports.homePage = (req, res) => {
-// 	res.render('hello', {
-// 		title: 'I Love Food! ',
-// 		name: 'AJ',
-// 		dog: req.query.dog || 'MAX',
-// 	});
-// };
+const multerOptions = {
+	// read the image into storage (as we'll be resizing the image and storing that in DB)
+	storage: multer.memoryStorage(),
+	fileFilter(req, file, next) {
+		// checking file type with extension can lead to virus file upload, that's why we check image via mimetype
+		const isPhoto = file.mimetype.startsWith('image/');
+		if (isPhoto) {
+			next(null, true);
+		} else {
+			next({ message: 'That filetype isn\'t allowed!'}, false);
+		}
+	}
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+	// check if there is no new file to resize
+	if (!req.file) {
+		next(); // skip to the next middleware
+		return;
+	}
+
+	const extension = req.file.mimetype.split('/')[1];
+	req.body.photo = `${uuid.v4()}.${extension}`;
+	// now we resize
+	const photo = await jimp.read(req.file.buffer);
+	await photo.resize(800, jimp.AUTO);
+	await photo.write(`./public/uploads/${req.body.photo}`);
+
+	// once we have written the photo in filesystem, keep going!
+	next();
+};
 
 exports.addStore = (req, res) => {
   res.render('editStore', {
