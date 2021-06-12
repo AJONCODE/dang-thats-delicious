063b3a17743c2bee90ec83482262ac8a01487019
ajoncode <ajoncode@gmail.com>
2021-06-12 19:06:20 +0530

    Geocoding Data with Google Maps

---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,20 @@
+function autocomplete(input, latInput, lngInput) {
+  if (!input) return; // skip this function from running if there is no input
+
+  const dropdown = new google.maps.places.Autocomplete(input);
+
+  dropdown.addListener('place_changed', () => {
+    const place = dropdown.getPlace();
+
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
+
+  // If someone hits enter on the address field, don't submit the form
+  input.on('keydown', (e) => {
+    // Key code for enter key is 13
+    if (e.keyCode === 13) e.preventDefault();
+  })
+}
+
+export default autocomplete;
