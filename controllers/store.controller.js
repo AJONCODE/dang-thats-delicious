9b3e913ac44334740ee8d4dc80b2bfb1c1ce556e
ajoncode <ajoncode@gmail.com>
2021-06-13 11:34:01 +0530

    Routing and Templating Single Stores

--- controllers/store.controller.js
+++ controllers/store.controller.js
@@ -100,3 +100,14 @@ exports.updateStore = async (req, res) => {
 
 	res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res) => {
+	const store = await Store.findOne({ slug: req.params.slug });
+
+	if(!store) return next(); // Now app.use(errorHandlers.notFound) in app.js will kick in
+
+	res.render('store', {
+		title: store.name,
+		store,
+	});
+};
--- routes/index.js
+++ routes/index.js
@@ -22,8 +22,10 @@ router.post(
   storeController.upload,
   catchErrors(storeController.resize),
   catchErrors(storeController.updateStore),
-  );
+);
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 module.exports = router;
